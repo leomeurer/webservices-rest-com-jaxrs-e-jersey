acb8554f35074a820b449348801fdc11874edb06
Leonardo Meurer <Leonardo@DESKTOP>
2022-07-15 16:12:13 -0300

    novo teste de servidor + refactor do código da inicialização do servidor

--- src/test/java/br/com/alura/loja/ClienteTest.java
+++ src/test/java/br/com/alura/loja/ClienteTest.java
@@ -4,10 +4,31 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import com.thoughtworks.xstream.XStream;
+
+import br.com.alura.loja.modelo.Carrinho;
+
 public class ClienteTest {
+
+	private HttpServer server;
+
+	@Before
+	public void before() {
+		server = Servidor.inicializaServidor();
+	}
+
+	@After
+	public void after() {
+
+		server.stop();
+		System.out.println("Servidor parou...");
+	}
 
 	@Test
 	public void testeQueAConexaoComOServidorEstaFuncionando() {
@@ -17,6 +38,18 @@ public class ClienteTest {
 		String conteudo = target.path("v2/52aaf5bbee7ba8c60329fb7b").request().get(String.class);
 
 		Assert.assertTrue(conteudo.contains("Rua"));
+	}
+
+	@Test
+	public void testeQueBuscaCarrinhoETrazOCarrinhoEsperado() {
+
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:8080");
+		String conteudo = target.path("/carrinhos").request().get(String.class);
+
+		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+
+		Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
 	}
 
 }
